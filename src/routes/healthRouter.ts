44fb23ca51efd08f725edc80042c27fa6fedfdf6
Larysse Silva <larysse.silva@19558-MB.local>
2025-05-12 11:26:37 -0300

    stile: configure lint

--- src/routes/healthRouter.ts
+++ src/routes/healthRouter.ts
@@ -3,7 +3,7 @@ import { Router, Request, Response } from 'express';
 const router = Router();
 
 router.get('/', (_req: Request, res: Response) => {
-    res.status(200).json({ status: 'OK' });
+  res.status(200).json({ status: 'OK' });
 });
 
 export default router;
--- src/routes/helloWorldRouter.spec.ts
+++ src/routes/helloWorldRouter.spec.ts
@@ -2,12 +2,12 @@ import request from 'supertest';
 import app from '../app';
 
 describe('HelloWorldRouter', () => {
-    describe('GET /hello-world', () => {
-        it('should return 200 OK', async () => {
-            const response = await request(app).get('/hello-world');
+  describe('GET /hello-world', () => {
+    it('should return 200 OK', async () => {
+      const response = await request(app).get('/hello-world');
 
-            expect(response.status).toBe(200);
-            expect(response.body).toEqual({ text: 'Hello World 2!' });
-        });
+      expect(response.status).toBe(200);
+      expect(response.body).toEqual({ text: 'Hello World 2!' });
     });
+  });
 });
